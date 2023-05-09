a5fad834a50c07742e11e66829d78a1ba37ef3bb
Beatriz <bia906@gmail.com>
2023-05-09 11:53:14 -0300

    application.properties fix

--- src/main/java/com/example/siteReceitas/models/UserAdmin.java
+++ src/main/java/com/example/siteReceitas/models/UserAdmin.java
@@ -7,8 +7,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Entity
 public class UserAdmin extends UserAbstract {
 
--- src/main/java/com/example/siteReceitas/models/UserPadrao.java
+++ src/main/java/com/example/siteReceitas/models/UserPadrao.java
@@ -6,8 +6,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Entity
 public class UserPadrao extends UserAbstract{
     @Override
