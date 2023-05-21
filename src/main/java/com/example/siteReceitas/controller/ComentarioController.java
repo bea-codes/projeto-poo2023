a70310e5e680ccd9dc84ccd693ce99d8bf443243
Gabriela Lima <gwabriel@gmail.com>
2023-05-21 12:35:37 -0300

    Comentario_Controller
    Comentario_Controller

--- src/main/java/com/example/siteReceitas/controller/ComentarioController.java
+++ src/main/java/com/example/siteReceitas/controller/ComentarioController.java
@@ -1,4 +1,38 @@
 package com.example.siteReceitas.controller;
 
+import java.util.stream.Collectors;
+
+import com.example.siteReceitas.dto.ComentarioDto;
+import com.example.siteReceitas.models.Comentario;
+import com.example.siteReceitas.repository.ComentarioRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/comentario")
+
 public class ComentarioController {
+
+    @Autowired
+    private ComentarioRepository comentarioRepository;
+
+        private ComentarioDto toDTO (Comentario comentario){
+
+            return  new ComentarioDto(comentario.getId(),
+                    comentario.getAutor(),
+                    comentario.getConteudo());
+        }
+
+        private Comentario toModel(ComentarioDto comentarioDto){
+            Comentario comentario = new Comentario();
+            comentario.setId(comentarioDto.getId());
+            comentario.setAutor(comentarioDto.getAutor());
+            comentario.setConteudo(comentarioDto.getConteudo());
+
+            return comentario;
+
+        }
+
 }
