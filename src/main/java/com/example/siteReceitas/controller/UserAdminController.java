b00a977e6d1f0db6e4fdde3575b634c1e052bc7e
Gabriela Lima <gwabriel@gmail.com>
2023-05-23 23:55:32 -0300

    teste

--- src/main/java/com/example/siteReceitas/controller/UserAdminController.java
+++ src/main/java/com/example/siteReceitas/controller/UserAdminController.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping(value="/Admin", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(value="/Admiin", produces = MediaType.APPLICATION_JSON_VALUE)
 public class UserAdminController {
     @Autowired
     public UserPadraoRepository userPadraoRepository;
