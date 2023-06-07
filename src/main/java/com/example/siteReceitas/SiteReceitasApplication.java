ed89f01c18a01ad255e0c11cf3fc2089c95cb07c
Beatriz <bia906@gmail.com>
2023-06-07 20:36:23 -0300

    main function @componentScan

--- src/main/java/com/example/siteReceitas/SiteReceitasApplication.java
+++ src/main/java/com/example/siteReceitas/SiteReceitasApplication.java
@@ -2,8 +2,10 @@ package com.example.siteReceitas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.example.siteReceitas.repository")
 public class SiteReceitasApplication {
 
 	public static void main(String[] args) {
