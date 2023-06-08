74b3de847a47a407a03e7e2d2843e15fe2ca413c
Beatriz <bia906@gmail.com>
2023-06-08 14:12:49 -0300

    Comentario Controller

--- src/main/java/com/example/siteReceitas/controller/ComentarioController.java
+++ src/main/java/com/example/siteReceitas/controller/ComentarioController.java
@@ -6,30 +6,46 @@ import com.example.siteReceitas.dto.ComentarioDto;
 import com.example.siteReceitas.models.Comentario;
 import com.example.siteReceitas.repository.ComentarioRepository;
 
+import com.example.siteReceitas.service.ComentarioService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/comentario")
 public class ComentarioController {
-
     @Autowired
-    private ComentarioRepository comentarioRepository;
+    private ComentarioService comentarioService;
 
-        private ComentarioDto toDTO (Comentario comentario){
-            return  new ComentarioDto(comentario.getId(),
-                    comentario.getAutor(),
-                    comentario.getConteudo());
-        }
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public ResponseEntity<List<Comentario>> readComentarios(){
+        return ResponseEntity.status(HttpStatus.OK).body(
+                comentarioService.getAllComentarios()
+        );
+    }
 
-        private Comentario toModel(ComentarioDto comentarioDto){
-            Comentario comentario = new Comentario();
-            comentario.setId(comentarioDto.getId());
-            comentario.setAutor(comentarioDto.getAutor());
-            comentario.setConteudo(comentarioDto.getConteudo());
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public ResponseEntity<Comentario> createComentario(@RequestBody Comentario comentario){
+        return ResponseEntity.status(HttpStatus.OK).body(comentarioService.createComentario(comentario));
+    }
 
-            return comentario;
-        }
+    @RequestMapping(value = "/delete/{comentario_id}")
+    public ResponseEntity<Comentario> deleteComentario(@PathVariable(value = "comentario_id") Long id){
+        comentarioService.deleteComentario(id);
+        return ResponseEntity.status(HttpStatus.OK).body(null);
+    }
+
+    @RequestMapping(value = "/update/{comentario_id}", method = RequestMethod.PUT)
+    public ResponseEntity<Comentario> updateComentario(@PathVariable(value = "comentario_id") Long id,
+                                                       @RequestBody Comentario comentario) throws ChangeSetPersister.NotFoundException {
+        return ResponseEntity.status(HttpStatus.OK).body(comentarioService.updateComentario(id, comentario));
+    }
+
+
+
 
 }
