349e33bb3a8a9badc583970d4003f7e58c8c7e9c
Beatriz <bia906@gmail.com>
2023-06-08 14:13:28 -0300

    Comentario Service

--- src/main/java/com/example/siteReceitas/service/ComentarioService.java
+++ src/main/java/com/example/siteReceitas/service/ComentarioService.java
@@ -4,6 +4,7 @@ package com.example.siteReceitas.service;
 import com.example.siteReceitas.models.Comentario;
 import com.example.siteReceitas.repository.ComentarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,10 +17,24 @@ public class ComentarioService {
     private ComentarioRepository comentarioRepository;
 
     public List<Comentario> getAllComentarios(){
-        return  comentarioRepository.findAll();
+        return comentarioRepository.findAll();
     }
 
     public Optional<Comentario> getComentarioById(Long id){
         return comentarioRepository.findById(id);
+    }
+
+    public Comentario createComentario(Comentario comentario){
+        return comentarioRepository.save(comentario);
+    }
+
+    public void deleteComentario(Long id){
+        comentarioRepository.deleteById(id);
+    }
+
+    public Comentario updateComentario(Long id, Comentario comentario) throws ChangeSetPersister.NotFoundException {
+        Comentario comentarioExistente = comentarioRepository.findById(id).orElseThrow(() -> new ChangeSetPersister.NotFoundException());
+        comentarioExistente.setConteudo(comentario.getConteudo());
+        return comentarioRepository.save(comentarioExistente);
     }
 }
